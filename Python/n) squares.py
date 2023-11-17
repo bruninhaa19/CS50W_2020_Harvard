21f06daac9fd872e1b8de812502e52f4f7cd9172
bruninhaa19 <br2020maia@gmail.com>
2023-11-17 13:54:31 -0300

    Create n) squares.py

---
+++ Python/n) squares.py
@@ -0,0 +1,4 @@
+from functions import square                               # 2) import functions
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")             # 2) print(f"The square of {i} is {functions.square(i)}")

52810072fe3d9517d38b1c35657f5cfd1e54e4fb
bruninhaa19 <br2020maia@gmail.com>
2024-01-16 10:05:59 -0300

    Update o) classes.py

--- Python/o) classes.py
+++ Python/o) classes.py
@@ -1,8 +1,8 @@
 class Point():
-    def __init__(self, input1, input2):
-        self.x = input1
-        self.y = input2
-
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
     p = Point(2, 8)
     print(p.x)
     print(p.y)
